db170cac848a51d99329864222aec24486b8022c
Darren Ballinger <darren.ballinger@gmail.com>
2013-11-09 16:44:21 +0000

    add test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,13 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
+describe('tutorial components', function () {
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+    beforeEach(module('tutorial'));
 
+    describe('ProductCtrl', function () {
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+        beforeEach(inject(function($controller, $rootScope) {
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+        }));
+    });
 });
