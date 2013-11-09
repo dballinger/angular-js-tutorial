a5726555166539d0d6207e5272af6d1950b7d5c5
Darren Ballinger <darren.ballinger@gmail.com>
2013-11-09 17:49:57 +0000

    added form directive

--- app/js/app.js
+++ app/js/app.js
@@ -21,3 +21,32 @@ angular.module('tutorial', [])
                 delete $scope.data['price'];
             };
         })
+
+        .directive('productForm', function () {
+            return {
+                replace:true,
+                restrict:'E',
+                template:
+                        '<form class="form-horizontal" ng-submit="add()">                                          '+
+                        '    <div class="form-group">                                                              '+
+                        '        <label for="product" class="col-lg-2 control-label">Product</label>               '+
+                        '                                                                                          '+
+                        '        <div class="col-lg-4">                                                            '+
+                        '            <input type="text" class="form-control" id="product" ng-model="data.name">    '+
+                        '        </div>                                                                            '+
+                        '    </div>                                                                                '+
+                        '    <div class="form-group">                                                              '+
+                        '        <label for="price" class="col-lg-2 control-label">Nett price</label>              '+
+                        '                                                                                          '+
+                        '        <div class="col-lg-2">                                                            '+
+                        '            <input type="text" class="form-control" id="price" ng-model="data.price">     '+
+                        '        </div>                                                                            '+
+                        '    </div>                                                                                '+
+                        '    <div class="form-group">                                                              '+
+                        '        <div class="col-sm-offset-2 col-sm-10">                                           '+
+                        '            <button type="submit" class="btn btn-primary">Add</button>                    '+
+                        '        </div>                                                                            '+
+                        '    </div>                                                                                '+
+                        '</form>                                                                                   '
+            }
+        })
