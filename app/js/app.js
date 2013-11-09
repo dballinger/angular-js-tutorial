5e0e74018e3b3d9076f6b7280b81f8ea77aaa7a7
Darren Ballinger <darren.ballinger@gmail.com>
2013-11-09 17:15:54 +0000

    added product controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 angular.module('tutorial', [])
+
+        .controller('ProductCtrl', function ($scope) {
+            $scope.data = {
+                products: [
+                    {name: "Mug", price: 1.5},
+                    {name: "Mini shopping cart", price: 2}
+                ]
+            };
+        })
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,8 +6,20 @@ describe('tutorial components', function () {
 
     describe('ProductCtrl', function () {
 
-        beforeEach(inject(function($controller, $rootScope) {
+        var scope;
 
+        beforeEach(inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            $controller('ProductCtrl', {$scope: scope});
         }));
+
+        it('should contain an array of existing products.', function() {
+            expect(scope.data.products).toEqual(
+                    [
+                        {name:"Mug", price:1.5},
+                        {name:"Mini shopping cart", price:2}
+                    ]
+            );
+        });
     });
 });
